3717a3e0454459e01875e1c82a79e87aaf7bcf6c
newt! <newtykins@outlook.com>
2021-09-16 21:06:32 +0100

    Draw pieces and make them moveable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,44 +1,114 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	public GameObject tilePrefab;
 	public GameObject board;
-	private int rowCount = 8;
-	private int tilePerRow = 8;
+	public GameObject piecePrefab;
+	public GameObject pieces;
+	public bool whiteTurn = true;
+	
+	private const int RowCount = 8;
+	private const int TilesPerRow = 8;
 
-	// Start is called before the first frame update
-	void Start()
+	private readonly List<string> _startingPosition = new List<string>()
 	{
+		"RNBQKBNR",
+		"PPPPPPPP",
+		"",
+		"",
+		"",
+		"",
+		"pppppppp",
+		"rnbqkbnr"
+	};
+
+	// Black Sprites
+	public Sprite blackRook;
+	public Sprite blackKnight;
+	public Sprite blackBishop;
+	public Sprite blackQueen;
+	public Sprite blackKing;
+	public Sprite blackPawn;
+	
+	// White Sprites
+	public Sprite whiteRook;
+	public Sprite whiteKnight;
+	public Sprite whiteBishop;
+	public Sprite whiteQueen;
+	public Sprite whiteKing;
+	public Sprite whitePawn;
+
+	public void Start()
+	{
+		// Reverse the starting position because I want the list to mirror the board in the code kljfsadjkhsa
+		_startingPosition.Reverse();
+		
+		// Set up the board
 		DrawTiles();
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
+		DrawPieces();
 	}
 
 	// Draws the board's tiles
 	private void DrawTiles()
 	{
-		for (var x =0; x < rowCount; x++)
+		for (var x = 0; x < RowCount; x++)
 		{
-			for (var y = 0; y < tilePerRow; y++)
+			for (var y = 0; y < TilesPerRow; y++)
 			{
 				// Create the tile
 				var tile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity, board.transform);
 				tile.name = $"{x},{y}";
 
-				// If the tile should not be dark, we are finished!
+				// Handle dark tiles
 				var isDark = (x + y) % 2 == 0;
-				if (!isDark) return;
-				
-				// Otherwise, make the tile dark
-				var spriteRenderer = tile.GetComponent<SpriteRenderer>();
-				ColorUtility.TryParseHtmlString("#d18b47", out var colour);
-				
-				spriteRenderer.color = colour;
+
+				if (isDark)
+				{
+					var spriteRenderer = tile.GetComponent<SpriteRenderer>();
+					ColorUtility.TryParseHtmlString("#b48866", out var colour);
+
+					spriteRenderer.color = colour;
+				}
+			}
+		}
+	}
+	
+	// Draws the board's pieces
+	private void DrawPieces()
+	{
+		var py = -1;
+		
+		foreach (var row in _startingPosition)
+		{
+			py++;
+			var px = -1;
+			
+			foreach (var code in row)
+			{
+				px++;
+				var piece = Instantiate(piecePrefab, new Vector3(px, py, -1), Quaternion.identity, pieces.transform);
+				var spriteRenderer = piece.GetComponent<SpriteRenderer>();
+
+				spriteRenderer.sprite = code switch
+				{
+					'R' => blackRook,
+					'N' => blackKnight,
+					'B' => blackBishop,
+					'Q' => blackQueen,
+					'K' => blackKing,
+					'P' => blackPawn,
+					'r' => whiteRook,
+					'n' => whiteKnight,
+					'b' => whiteBishop,
+					'q' => whiteQueen,
+					'k' => whiteKing,
+					'p' => whitePawn,
+					_ => spriteRenderer.sprite
+				};
 			}
 		}
 	}
 }
+
