84891c0aa6bc62dc6fad57471e8d566eee6fbeff
newt! <newtykins@outlook.com>
2021-09-17 22:49:12 +0100

    Allow pieces to be taken

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
@@ -6,11 +7,15 @@ public class Piece : MonoBehaviour
     private Vector3 _mousePosition;
     private Vector3 _screenPoint;
     private Vector3 _oldPiecePosition;
+    private bool _isWhite;
 
     public void Start()
     {
         // Find and assign the game manager
         _manager = GameObject.FindWithTag("Manager").GetComponent<GameManager>();
+        
+        // Determine the piece's colour
+        _isWhite = gameObject.name.Contains("White");
     }
     
     public void OnMouseDown()
@@ -32,12 +37,39 @@ public class Piece : MonoBehaviour
     {
         var px = Mathf.Round(_mousePosition.x);
         var py = Mathf.Round(_mousePosition.y);
+        var isInBounds = (0 < px && px <= 8) && (0 < py && py <= 8);
 
         // Ensure the position is in bounds of the board
-        if ((0 < px && px <= 8) && (0 < py && py <= 8))
+        if (isInBounds)
         {
             // Snap the piece
             transform.position = new Vector3(px, py);
+
+            // Manage the taking of pieces!
+            var allPieces = GameObject.FindGameObjectsWithTag("Pieces");
+            
+            foreach (var piece in allPieces)
+            {
+                // Ensure that the piece does not delete itself
+                if (piece.transform.position != transform.position || piece == gameObject) continue;
+                
+                if (piece.GetComponent<Piece>()._isWhite && _isWhite)
+                {
+                    transform.position = _oldPiecePosition;
+                    return;
+                }
+
+                if (!piece.GetComponent<Piece>()._isWhite && !_isWhite)
+                {
+                    transform.position = _oldPiecePosition;
+                    return;
+                }
+
+                Destroy(piece);
+            }
+
+            // Invert the turn variable
+            _manager.whiteTurn ^= true;
         }
         else
         {
