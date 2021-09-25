93ff263b80e8d9bdcc92d9662001ec02f2b772a7
newt! <newtykins@outlook.com>
2021-09-25 14:34:37 +0100

    Add place sounds!

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject placeSoundStore;
+
+    private AudioSource[] _placeSounds;
+
+    public void Start()
+    {
+        _placeSounds = placeSoundStore.GetComponents<AudioSource>();
+    }
+
+    public void PlayPlace()
+    {
+        var placeSound = RandomSound(_placeSounds);
+        placeSound.Play();
+    }
+
+    private static AudioSource RandomSound(IReadOnlyList<AudioSource> sounds)
+    {
+        var random = new System.Random();
+        var index = random.Next(sounds.Count);
+
+        return sounds[index];
+    }
+}
