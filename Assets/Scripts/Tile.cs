f94b92663b5badd08473def9cb4c800c7e742ab3
newt <hi@newty.dev>
2021-10-04 20:48:43 +0100

    Add last move highlights

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-	public bool _isLight;
-	public bool _highlighted = false;
-	public Color32 _defaultColour;
+	public bool isLight;
+	public bool indicator = false;
+	public bool highlighted = false;
+	public Color32 defaultColour;
+	public Color32 currentColour;
 	private SpriteRenderer _spriteRenderer;
 
 	public void Start()
@@ -14,10 +16,11 @@ public class Tile : MonoBehaviour
 
 		// Figure out the tiles colours
 		var position = transform.position;
-		_isLight = (position.x + position.y) % 2 == 0;
+		isLight = (position.x + position.y) % 2 == 0;
 
-		_defaultColour = _isLight ? new Color32(240, 217, 183, 255) : new Color32(180, 136, 102, 255);
-		_spriteRenderer.color = _defaultColour;
+		defaultColour = isLight ? new Color32(240, 217, 183, 255) : new Color32(180, 136, 102, 255);
+		_spriteRenderer.color = defaultColour;
+		currentColour = _spriteRenderer.color;
 
 		// Name the tile
 		gameObject.name = $"({position.x}, {position.y})";
