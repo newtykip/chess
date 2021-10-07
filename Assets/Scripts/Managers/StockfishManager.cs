e0b4814307eb50d6281ce69dc96fdb9be997c35c
newt! <newtykins@outlook.com>
2021-10-08 00:52:57 +0100

    Add a Stockfish toggle

--- Assets/Scripts/Managers/StockfishManager.cs
+++ Assets/Scripts/Managers/StockfishManager.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class StockfishManager : MonoBehaviour
 {
+	public TextMeshProUGUI toggleButtonText;
 	private GameManager _gameManager;
 	private NETfish.Stockfish _stockfish;
-	private char[] _alphabet;
 	public bool isEnabled = false;
 	public bool isWhite = false;
 
@@ -14,7 +15,6 @@ public class StockfishManager : MonoBehaviour
 	{
 		// Find the game manager
 		_gameManager = GameObject.FindWithTag("Manager").GetComponent<GameManager>();
-		_alphabet = _gameManager.alphabet;
 
 		// Start Stockfish
 		_stockfish = new NETfish.Stockfish($"{Application.streamingAssetsPath}/stockfish14.exe");
@@ -27,12 +27,18 @@ public class StockfishManager : MonoBehaviour
 		var k = 0f;
 		var stockfishMove = _stockfish.GetBestMove().ToLookup(c => Math.Floor(k++ / 2)).Select(e => new string(e.ToArray())).ToArray();
 
-		var stockfishFrom = new Vector2(Array.IndexOf(_alphabet, stockfishMove[0][0]) + 1, 0);
+		var stockfishFrom = new Vector2(Array.IndexOf(_gameManager.alphabet, stockfishMove[0][0]) + 1, 0);
 		float.TryParse(stockfishMove[0][1].ToString(), out stockfishFrom.y);
 
-		var stockfishTo = new Vector2(Array.IndexOf(_alphabet, stockfishMove[1][0]) + 1, 0);
+		var stockfishTo = new Vector2(Array.IndexOf(_gameManager.alphabet, stockfishMove[1][0]) + 1, 0);
 		float.TryParse(stockfishMove[1][1].ToString(), out stockfishTo.y);
 
 		return new MoveSet(stockfishFrom, stockfishTo);
+	}
+
+	public void Toggle()
+	{
+		isEnabled = !isEnabled;
+		toggleButtonText.text = isEnabled ? "Disable Stockfish" : "Enable Stockfish";
 	}
 }
